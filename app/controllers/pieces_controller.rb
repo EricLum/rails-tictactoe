6f5af1714171d0fb17f733df69098490ac4de1a8
eal6gf <elum1234@gmail.com>
2017-12-18 06:33:39 -0500

    figured post logic

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,8 +1,12 @@
 class PiecesController < ApplicationController
 
   def create
+    #make piece association with gameboard
     @piece = Piece.create(gameboard_id: params[:gameboard][:id], xposition: params[:piece][:xposition], yposition: params[:piece][:yposition])
+
+    #not sure why i even have this.
     @gameBoard = Gameboard.find_by_id(params[:gameboard][:id])
+
     
   end
 
